f2934bedf1fb07d2ce0df54333d1856b1ae5766a
zhontao <zhontao@ZHONTAO1.asiapacific.hpqcorp.net>
2016-01-29 14:11:07 +0800

    c

--- src/main/java/com/microsoft/ews/Test.java
+++ src/main/java/com/microsoft/ews/Test.java
@@ -1,23 +1,57 @@
 package com.microsoft.ews;
 
+import java.io.IOException;
+
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.internet.MimeMultipart;
-
-import org.bouncycastle.cms.CMSException;
-import org.bouncycastle.mail.smime.SMIMESigned;
 
 public class Test {
   public static void main(String args[]) {
     try {
       DataSource source = new FileDataSource("C:/Users/zhontao/Desktop/smime.p7m");
-      MimeMultipart multi = new MimeMultipart(source);
-      SMIMESigned signedData = new SMIMESigned(multi);
+      MimeMultipart multi1 = new MimeMultipart(source);
+      for (int i = 0; i < multi1.getCount(); i++) {
+        Part part1 = multi1.getBodyPart(i);
+        if (part1.getContent() instanceof Multipart) {
+          Multipart multi2 = (Multipart) part1.getContent();
+          for (int j = 0; j < multi2.getCount(); j++) {
+            Part part2 = multi2.getBodyPart(i);
+            String contentType = part2.getContentType();
+            System.out.println(contentType);
+            // generally if the content type multipart/alternative, it is email text.
+            if (part2.isMimeType("multipart/alternative")) {
+              if (part2.getContent() instanceof Multipart) {
+                Multipart multi3 = (Multipart) part2.getContent();
+                for (int k = 0; k < multi3.getCount(); k++) {
+                  Part part4 = multi3.getBodyPart(k);
+                  String contentType1 = part4.getContentType();
+                  System.out.println(contentType1);
+                  if (part4.isMimeType("text/plain") && !Part.ATTACHMENT.equalsIgnoreCase(part4.getDisposition())) {
+                    System.out.println(part4.getContent().toString());
+                  } else if (part4.isMimeType("text/html") && !Part.ATTACHMENT.equalsIgnoreCase(part4.getDisposition())) {
+                    System.out.println(part4.getContent().toString());
+                  }
+                }
+              }
+            }
+          }
+        } else {
+          String contentType = part1.getContentType();
+          System.out.println(contentType);
+          String disposition = part1.getDisposition();
+          System.out.println(disposition);
+          System.out.println(part1.getFileName());
+          // this.saveFile(part1.getFileName(), part1.getInputStream());
+        }
+      }
     } catch (MessagingException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
-    } catch (CMSException e) {
+    } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
